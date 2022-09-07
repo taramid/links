5b22f785f99ca6c24d961c3a3c950b2000ecc688
Dmytro Tverdokhlib <ditverd@gmail.com>
2022-09-07 15:34:56 +0300

    php artisan make:model -fcrm

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Hidehalo\Nanoid\Client;
+use Hidehalo\Nanoid\GeneratorInterface;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Link>
+ */
+class LinkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $nano = new Client();
+
+        return [
+            //
+        ];
+    }
+}
