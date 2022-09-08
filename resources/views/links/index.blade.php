90d5faaad795549e15095f8678f84ad656db72ff
Dmytro Tverdokhlib <ditverd@gmail.com>
2022-09-08 17:32:05 +0300

    Layout, Link components

---
+++ resources/views/links/index.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+
+    @if(empty($links))
+
+        <p>there's no any link yet</p>
+
+    @else
+        <table>
+        @foreach($links as $link)
+            <tr>
+                <td class="mx-1">{{ $link->hook }}</td>
+                <td class="mx-1">{{ $link->url }}</td>
+            </tr>
+        @endforeach
+        </table>
+    @endif
+
+</x-layout>
