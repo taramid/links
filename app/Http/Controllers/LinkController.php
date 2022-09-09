14159d38ef1d46f9d634827275df2c7d71b66a2d
Dmytro Tverdokhlib <ditverd@gmail.com>
2022-09-10 00:56:56 +0300

    -

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -17,7 +17,10 @@ class LinkController extends Controller
     public function index()
     {
         return response()->view('links.index', [
-            'links' => Link::select('links.id','links.hook','links.url')->orderBy('id', 'desc')->limit(env('INDEX_CHUNK', 44))->get()
+            'links' => Link::select('links.id','links.hook','links.url')
+                ->orderBy('id', 'desc')
+                ->limit(env('INDEX_CHUNK', 44))
+                ->get()
         ]);
     }
 
